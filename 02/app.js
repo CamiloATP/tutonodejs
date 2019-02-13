56c33ea469d487f9a06fa0e90b27d6a365fb93b3
Camilo <camilo.antonio.t@gmail.com>
2019-02-13 11:16:06 -0300

    gitignore

---
+++ 02/app.js
@@ -0,0 +1,15 @@
+// Se crea una variable donde se guardará la libreria express.js
+var express = require('express');
+
+// Se asigna la app de tipo express para gestionar rutas y demás...
+var app = express();
+
+// Se gestionan las rutas..
+app.get('/', function(req, res){
+  res.send("Hola mundo con express.js");
+});
+
+
+app.listen(3000, function(){
+  console.log('La app esta funcionando en el puerto 3000');
+});
